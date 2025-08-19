7d75515b167322d407a59d7e566d6bdfa6dca257
enkay <khoankn@coin98.finance>
2025-08-19 16:13:11 +0700

    refactor(varaiable name): remove underscore

--- programs/token_synchronize/src/helper/spl_tokens.rs
+++ programs/token_synchronize/src/helper/spl_tokens.rs
@@ -27,8 +27,8 @@ use std::cmp::min;
 use crate::error::ErrorCode;
 
 fn compute_expected_fee(mint_transfer_fee: &TransferFeeConfig, amount: u64) -> Result<u64> {
-    let _current_epoch: u64 = Clock::get()?.epoch.into();
-    let (_basis_points, _max_fee): (u16, u64) = if _current_epoch >= mint_transfer_fee.newer_transfer_fee.epoch.into() {
+    let current_epoch: u64 = Clock::get()?.epoch.into();
+    let (basis_points, max_fee): (u16, u64) = if current_epoch >= mint_transfer_fee.newer_transfer_fee.epoch.into() {
         (
             mint_transfer_fee
                 .newer_transfer_fee
@@ -52,27 +52,27 @@ fn compute_expected_fee(mint_transfer_fee: &TransferFeeConfig, amount: u64) -> R
         )
     };
 
-    let _raw_fee = (amount as u128) * (_basis_points as u128) / (10_000 as u128);
-    let _expected_fee = min(_raw_fee as u64, _max_fee);
-    Ok(_expected_fee)
+    let raw_fee = (amount as u128) * (basis_points as u128) / (10_000 as u128);
+    let expected_fee = min(raw_fee as u64, max_fee);
+    Ok(expected_fee)
 }
 
 fn append_hook_accounts<'info>(ix: &mut Instruction, infos: &mut Vec<AccountInfo<'info>>, extra_accounts: &[AccountInfo<'info>], hook_program_id: Pubkey) -> Result<()> {
     require!(!extra_accounts.is_empty(), ErrorCode::InvalidExtraAccounts);
 
-    let _hook_program_ai = &extra_accounts[0];
-    require_keys_eq!(*_hook_program_ai.key, hook_program_id, ErrorCode::InvalidExtraAccounts);
-    require!(_hook_program_ai.executable, ErrorCode::InvalidExtraAccounts);
+    let hook_program_ai = &extra_accounts[0];
+    require_keys_eq!(*hook_program_ai.key, hook_program_id, ErrorCode::InvalidExtraAccounts);
+    require!(hook_program_ai.executable, ErrorCode::InvalidExtraAccounts);
 
-    let mut _metas = ix.accounts.clone();
-    for _account in extra_accounts.iter() {
-        match _account.is_writable {
-            true => _metas.push(AccountMeta::new(*_account.key, _account.is_signer)),
-            false => _metas.push(AccountMeta::new_readonly(*_account.key, _account.is_signer)),
+    let mut metas = ix.accounts.clone();
+    for account in extra_accounts.iter() {
+        match account.is_writable {
+            true => metas.push(AccountMeta::new(*account.key, account.is_signer)),
+            false => metas.push(AccountMeta::new_readonly(*account.key, account.is_signer)),
         }
     }
 
-    ix.accounts = _metas;
+    ix.accounts = metas;
     infos.extend_from_slice(extra_accounts);
 
     Ok(())
@@ -92,37 +92,37 @@ pub fn transfer<'info>(
     if amount == 0 {
         return Ok(());
     }
-    let _pid = token_program_id.key();
+    let pid = token_program_id.key();
 
-    require_eq!(*mint.owner, _pid, ErrorCode::InvalidTokenProgram);
-    require_keys_eq!(*source.owner, _pid, ErrorCode::InvalidTokenProgram);
-    require_keys_eq!(*destination.owner, _pid, ErrorCode::InvalidTokenProgram);
+    require_eq!(*mint.owner, pid, ErrorCode::InvalidTokenProgram);
+    require_keys_eq!(*source.owner, pid, ErrorCode::InvalidTokenProgram);
+    require_keys_eq!(*destination.owner, pid, ErrorCode::InvalidTokenProgram);
 
-    let mut _ix: Instruction;
-    let mut _infos: Vec<AccountInfo<'info>> = vec![
+    let mut ix: Instruction;
+    let mut infos: Vec<AccountInfo<'info>> = vec![
         source.clone(),
         mint.clone(),
         destination.clone(),
         authority.clone(),
     ];
 
-    let _signer_keys: Vec<Pubkey> = signer_accounts.iter().map(|a: &AccountInfo<'info>| *a.key).collect();
-    let _signer_pubkeys: Vec<&Pubkey> = _signer_keys.iter().collect();
+    let signer_keys: Vec<Pubkey> = signer_accounts.iter().map(|a: &AccountInfo<'info>| *a.key).collect();
+    let signer_pubkeys: Vec<&Pubkey> = signer_keys.iter().collect();
 
-    if signer_accounts.is_empty(){
+    if signer_accounts.is_empty() && authority_seeds.is_empty() {
         require!(authority.is_signer, ErrorCode::AuthorityNotSigner);
     } else {
-        for _signer in signer_accounts.iter() {
-            require!(_signer.is_signer, ErrorCode::MissingSigner);
+        for signer in signer_accounts.iter() {
+            require!(signer.is_signer, ErrorCode::MissingSigner);
         }
-        _infos.extend(signer_accounts.iter().cloned());
+        infos.extend(signer_accounts.iter().cloned());
     }
 
-    let _data = mint.try_borrow_data()?;
+    let data = mint.try_borrow_data()?;
 
-    match _pid {
-        _id if _id == spl_token::ID => {
-            let _mint = spl_token::state::Mint::unpack(_data.as_ref())?;
+    match pid {
+        id if id == spl_token::ID => {
+            let mint_token = spl_token::state::Mint::unpack(data.as_ref())?;
 
             let source_account = spl_token::state::Account::unpack(&source.data.borrow())?;
             let destination_account = spl_token::state::Account::unpack(&destination.data.borrow())?;
@@ -132,19 +132,19 @@ pub fn transfer<'info>(
 
             require!(source_account.amount >= amount, ErrorCode::InsufficientFunds);
 
-            _ix = spl_token::instruction::transfer_checked(
-                &_pid,
+            ix = spl_token::instruction::transfer_checked(
+                &pid,
                 &source.key(),
                 &mint.key(),
                 &destination.key(),
                 &authority.key(),
-                &_signer_pubkeys,
+                &signer_pubkeys,
                 amount,
-                _mint.decimals,
+                mint_token.decimals,
             )?;
         }
-        _id if _id == spl_token_2022::ID => {
-            let _mint: StateWithExtensions<'_, Mint> = StateWithExtensions::<Mint>::unpack(_data.as_ref())?;
+        id if id == spl_token_2022::ID => {
+            let mint_token: StateWithExtensions<'_, Mint> = StateWithExtensions::<Mint>::unpack(data.as_ref())?;
 
             let source_data = source.try_borrow_data()?;
             let destination_data = destination.try_borrow_data()?;
@@ -160,44 +160,44 @@ pub fn transfer<'info>(
 
             require!(source_account.base.amount >= amount, ErrorCode::InsufficientFunds);
 
-            if let Ok(_mint_transfer_fee) = _mint.get_extension::<TransferFeeConfig>() {
-                _ix = spl_token_2022::extension::transfer_fee::instruction::transfer_checked_with_fee(
-                    &_pid,
+            if let Ok(mint_transfer_fee) = mint_token.get_extension::<TransferFeeConfig>() {
+                ix = spl_token_2022::extension::transfer_fee::instruction::transfer_checked_with_fee(
+                    &pid,
                     &source.key(),
                     &mint.key(),
                     &destination.key(),
                     &authority.key(),
-                    &_signer_pubkeys,
+                    &signer_pubkeys,
                     amount,
-                    _mint.base.decimals,
-                    compute_expected_fee(_mint_transfer_fee, amount)?,
+                    mint_token.base.decimals,
+                    compute_expected_fee(mint_transfer_fee, amount)?,
                 )?;
             } else {
-                _ix = spl_token_2022::instruction::transfer_checked(
-                    &_pid,
+                ix = spl_token_2022::instruction::transfer_checked(
+                    &pid,
                     &source.key(),
                     &mint.key(),
                     &destination.key(),
                     &authority.key(),
-                    &_signer_pubkeys,
+                    &signer_pubkeys,
                     amount,
-                    _mint.base.decimals,
+                    mint_token.base.decimals,
                 )?;
             }
 
-            if let Ok(_mint_transfer_hook) = _mint.get_extension::<TransferHook>() {
-                let _hook_program_id = _mint_transfer_hook.program_id.0;
-                drop(_data);
+            if let Ok(mint_transfer_hook) = mint_token.get_extension::<TransferHook>() {
+                let hook_program_id = mint_transfer_hook.program_id.0;
+                drop(data);
 
-                append_hook_accounts(&mut _ix, &mut _infos, &extra_accounts, _hook_program_id)?;
+                append_hook_accounts(&mut ix, &mut infos, &extra_accounts, hook_program_id)?;
             }
         }
         _ => return Err(error!(ErrorCode::InvalidTokenProgram)),
     }
 
     match authority_seeds.is_empty() {
-        true => invoke(&_ix, &_infos)?,
-        false => invoke_signed(&_ix, &_infos, &authority_seeds)?,
+        true => invoke(&ix, &infos)?,
+        false => invoke_signed(&ix, &infos, &authority_seeds)?,
     }
 
     Ok(())
